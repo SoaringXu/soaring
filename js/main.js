84028244846538a2b1490afcce73cadde8bc4b71
xuzhengxiang <xuzhengxiang@xbwl.tech>
2025-04-22 15:33:29 +0800

    feat: 新增滑块挑战游戏。

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 urls = [
+    ["滑块挑战", "./urls/slider-challenge.html"],
     ["生命起源", "./urls/life_simulation.html"],
     ["幻术图片", "./urls/mix_png.html"],
-    ["叠塔小游戏", "./towers.html"],
+    ["叠塔游戏", "./towers.html"],
     ["互联网黑话", "./blackwords.html"],
 ]
 for (var i = 0; i < urls.length; i++) {
