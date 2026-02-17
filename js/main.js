540a2dabf4868abefb090eb3d51e22fdb5ab190a
xuzhengxiang <xuzhengxiang@xbwl.tech>
2026-02-17 22:40:34 +0800

    feat: 新增吃喝玩乐项目列表；

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 urls = [
     ["记忆大师", "./memory-master.html"],
     ["俄罗斯方块", "./urls/tetris.html"],
+    ["字幕图片", "./urls/subtitle_generator.html"],
     ["滑块挑战", "./urls/slider-challenge.html"],
     ["生命起源", "./urls/life_simulation.html"],
     ["幻术图片", "./urls/mix_png.html"],
